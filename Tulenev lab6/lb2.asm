19730786e73923c583bc8bac8f6aa1cb7f7969ea
Timofey02 <71717477+Timofey02@users.noreply.github.com>
2022-06-08 15:27:30 +0300

    Update lb2.asm

--- Tulenev lab6/lb2.asm
+++ Tulenev lab6/lb2.asm
@@ -1,233 +1,229 @@
-TESTPC SEGMENT
-	ASSUME CS:TESTPC, DS:TESTPC, ES:NOTHING, SS:NOTHING
-	ORG 100H
+PC  Segment
+        Assume CS:PC, DS:PC, ES:NOTHING, SS:NOTHING
+        ORG 100H
+
 START: JMP BEGIN
 
-UNAVAILABLE_MEMORY db 'Address of unavailable memory segment:     ', 0DH, 0AH, '$'
-ENVIRONMENT db 'Address of environment segment:     ', 0DH, 0AH, '$'
-CONTENT_ENV_AREA db 'Contents of environment area:   ', 0DH, 0AH, '$'
-COMMAND_LINE_END_EMPTY db 'End of command line: empty', 0DH, 0AH, '$'
-COMMAND_LINE_END db 'End of command line:$'
-LOADED_MODULE_PATH db 'Path of loaded module:$'
+;ÄÀÍÍÛÅ
+
+Unavailable_Memory_Msg db 'Unavailable memory address:     ', 0ah, '$'
+Segment_Env_Addres_Msg db 'Environment address:     ', 0ah, '$'
+Input_String db 'Input string:', '$'
+
+;ÏÐÎÖÅÄÓÐÛ
 
 TETR_TO_HEX PROC near
-		and AL, 0Fh
-		cmp AL, 09
-		jbe NEXT
-		add AL, 07
-NEXT:	add AL, 30h
-		ret
+   and AL,0Fh
+   cmp AL,09
+   jbe next
+   add AL,07
+next:
+   add AL,30h
+   ret
 TETR_TO_HEX ENDP
 
-BYTE_TO_HEX	PROC near
-		push CX
-		mov AH, AL
-		call TETR_TO_HEX
-		xchg AL, AH
-		mov CL, 4
-		shr AL, CL
-		call TETR_TO_HEX
-		pop CX
-		ret
+BYTE_TO_HEX PROC near ;áàéò â AL ïåðåâîäèòñÿ â äâà ñèìâîëà øåñò. ÷èñëà â AX
+   push CX
+   mov AH,AL
+   call TETR_TO_HEX
+   xchg AL,AH
+   mov CL,4
+   shr AL,CL
+   call TETR_TO_HEX ;â AL ñòàðøàÿ öèôðà
+   pop CX ;â AH ìëàäøàÿ
+   ret
 BYTE_TO_HEX ENDP
 
-WRD_TO_HEX PROC near
-		push BX
-		mov BH, AH
-		call BYTE_TO_HEX
-		mov [DI], AH
-		dec DI
-		mov [DI], AL
-		dec DI
-		mov AL, BH
-		call BYTE_TO_HEX
-		mov [DI], AH
-		dec DI
-		mov [DI], AL
-		pop BX
-		ret
+WRD_TO_HEX PROC near ;ïåðåâîä â 16 ñ/ñ 16-òè ðàçðÿäíîãî ÷èñëà
+					 ; â AX - ÷èñëî, DI - àäðåñ ïîñëåäíåãî ñèìâîëà
+   push BX
+   mov BH,AH
+   call BYTE_TO_HEX
+   mov [DI],AH
+   dec DI
+   mov [DI],AL
+   dec DI
+   mov AL,BH
+   call BYTE_TO_HEX
+   mov [DI],AH
+   dec DI
+   mov [DI],AL
+   pop BX
+   ret
 WRD_TO_HEX ENDP
 
-BYTE_TO_DEC PROC near
-		push CX
-		push DX
-		xor AH, AH
-		xor DX, DX
-		mov CX, 10
-loop_bd:	div CX
-		or DL, 30h
-		mov [SI], DL
-		dec SI
-		xor DX, DX
-		cmp AX, 10
-		jae loop_bd
-		cmp AL, 00h
-		je end_l
-		or AL, 30h
-		mov [SI], AL
-end_l:	pop DX
-		pop CX
-		ret
+BYTE_TO_DEC PROC near ; ïåðåâîä â 10ñ/ñ, SI - àäðåñ ïîëÿ ìëàäøåé öèôðû
+   push CX
+   push DX
+   xor AH,AH
+   xor DX,DX
+   mov CX,10
+loop_bd:
+   div CX
+   or DL,30h
+   mov [SI],DL
+   dec SI
+   xor DX,DX
+   cmp AX,10
+   jae loop_bd
+   cmp AL,00h
+   je end_l
+   or AL,30h
+   mov [SI],AL
+end_l:
+   pop DX
+   pop CX
+   ret
 BYTE_TO_DEC ENDP
 
-WRITE_MESSAGE_WORD PROC near
-	push AX
-	mov AH, 09h
-	int 21h
-	pop AX
-	ret
-WRITE_MESSAGE_WORD ENDP
+print PROC NEAR
+  push ax
+  mov ah, 09h
+  int 21h
+  pop ax
+  ret
+print ENDP
 
-WRITE_MESSAGE_BYTE PROC near
-	push AX
-	mov AH, 02h
-	int 21h
-	pop AX
-	ret
-WRITE_MESSAGE_BYTE ENDP
+Prnt_Unavailable proc near
+  push ax
+  push di
+  push dx
 
-PRINT_UNAVAILABLE_MEMORY PROC near
-	push AX
-	push DI
-	push DX
+  mov ax, es:[02h]
+  mov di, offset Unavailable_Memory_Msg
+  add di, 31
+  call wrd_to_hex
+  mov dx, offset Unavailable_Memory_Msg
+  call print
 
-	mov AX, DS:[02h]
-	mov DI, offset UNAVAILABLE_MEMORY
-	add DI, 42
-	call WRD_TO_HEX
-	mov DX, offset UNAVAILABLE_MEMORY
+  pop dx
+  pop di
+  pop ax
+  ret
+Prnt_Unavailable endp
 
-	call WRITE_MESSAGE_WORD
-	
-	pop DX
-	pop DI
-	pop AX
+Prnt_Env_Address proc near
+  push ax
+  push di
+  push dx
 
-	ret
-PRINT_UNAVAILABLE_MEMORY ENDP
+  mov ax, es:[02Ch]
+  mov di, offset Segment_Env_Addres_Msg
+  add di, 24
+  call wrd_to_hex
+  mov dx, offset Segment_Env_Addres_Msg
+  call print
 
-PRINT_ENVIRONMENT PROC near
-	push AX
-	push DI
-	push DX
+  pop dx
+  pop di
+  pop ax
+  ret
+Prnt_Env_Address endp
 
-	mov AX, DS:[02Ch]
-	mov DI, offset ENVIRONMENT
-	add DI, 35
-	call WRD_TO_HEX
-	mov DX, offset ENVIRONMENT
+Prnt_Input_String proc near
+  push dx
+  push cx
+  push si
+  push ax
 
-	call WRITE_MESSAGE_WORD
+  mov dx, offset Input_String
+  call print
+  mov cl, ds:[80h]
+  mov si, 081h
+  mov ah, 02h  
+  cmp cl, 0
+  je end_
 
-	pop DX
-	pop DI
-	pop AX
 
-	ret
-PRINT_ENVIRONMENT ENDP
+  print_symbol:
+    mov dl, [si]
+    int 21h
+    inc si
+    loop print_symbol
 
-PRINT_COMMAND_LINE_END PROC near
-	push AX
-	push DI
-	push CX
-	push DX
 
-	xor CX, CX
+  end_:
+    mov dl, 0ah
+    int 21h
+  
+  pop ax
+  pop si
+  pop cx
+  pop dx
 
-	mov CL, DS:[80h]
-	cmp CL, 0h
-	je empty_cont
-	xor DI, DI
+  ret
 
-	mov DX, offset COMMAND_LINE_END
-	call WRITE_MESSAGE_WORD
+Prnt_Input_String endp
 
-	cycle:
-		mov DL, DS:[81h+DI]
-		call WRITE_MESSAGE_BYTE
-		inc DI
-	loop cycle
-	mov DL, 0Dh
-	call WRITE_MESSAGE_BYTE
-	mov DL, 0Ah
-	call WRITE_MESSAGE_BYTE
-	jmp final
-		
-	empty_cont:
-		mov DX, offset COMMAND_LINE_END_EMPTY
-		call WRITE_MESSAGE_WORD
+Prnt_String proc near
+  push dx
+  push ax
+  mov ah, 02h
 
-	final:
-		pop DX
-		pop CX
-		pop DI
-		pop AX
-	ret
-PRINT_COMMAND_LINE_END ENDP
+  print_sym:
+    mov dl, ds:[si]
+    inc si
+      
+    cmp dl, 0
+    jz end_of_string
+      
+    int 21h
+    jmp print_sym 
 
-PRINT_CONTENT_ENV_AREA_AND_LOADED_MODULE_PATH PROC near
-	push AX
-	push DI
-	push DX
-	push ES
+  end_of_string:
+    mov dl, 0ah
+    int 21h
 
-	mov DX, offset CONTENT_ENV_AREA
-	call WRITE_MESSAGE_WORD
-	xor DI, DI
-	mov AX, DS:[2Ch]
-	mov ES, AX
+  pop ax
+  pop dx
+  ret
+Prnt_String endp
 
-	cycle_02:
-		mov DL, ES:[DI]
-		cmp DL, 0h
-		je end_word
-		call WRITE_MESSAGE_BYTE
-		inc DI
-		jmp cycle_02
+Prnt_Environment_Content_And_Path proc near
+  push ds
+  push si
+  push ax
+  push cx
+  push dx
 
-	end_word:
-		mov DL, 0Ah
-		call WRITE_MESSAGE_BYTE
-		inc DI
-		mov DL, ES:[DI]
-		cmp DL, 0h
-		je final_02
-		call WRITE_MESSAGE_BYTE
-		inc DI
-		jmp cycle_02
-	
-	final_02:
-		mov DX, offset LOADED_MODULE_PATH
-		call WRITE_MESSAGE_WORD
-		add DI, 3
-		cycle_03:
-			mov DL, ES:[DI]
-			cmp DL, 0h
-			je final_03
-			call WRITE_MESSAGE_BYTE
-			inc DI
-			jmp cycle_03
+  mov ds, es:[2ch]
+  mov si, 0
 
-	final_03:
-		pop ES
-		pop DX
-		pop DI
-		pop AX
-	ret
-PRINT_CONTENT_ENV_AREA_AND_LOADED_MODULE_PATH ENDP
+  Prnt_Strings:
+    call Prnt_String
+    mov bl, ds:[si]
+    cmp bl, 0
+    jz print_path
+    jmp Prnt_Strings
+
+  print_path:
+    add si, 3 
+    call Prnt_String
+
+  the_end:
+    pop dx
+    pop cx
+    pop ax
+    pop si
+    pop ds
+
+  ret
+
+Prnt_Environment_Content_And_Path endp
 
 BEGIN:
-	call PRINT_UNAVAILABLE_MEMORY
-	call PRINT_ENVIRONMENT
-	call PRINT_COMMAND_LINE_END
-	call PRINT_CONTENT_ENV_AREA_AND_LOADED_MODULE_PATH
 
-	xor AL, AL
-	
-	mov AH, 01h ;запросить с клавиатуры символ и поместить введенный символ в регистр AL
-	int 21h
-	
-	mov AH, 4Ch
-	int 21h
-TESTPC ENDS
-END START  
+  call Prnt_Unavailable
+  call Prnt_Env_Address
+  call Prnt_Input_String
+  call Prnt_Environment_Content_And_Path
+
+  xor ax, ax
+  mov ah, 01h
+  int 21h
+
+  mov ah, 4Ch
+  int 21h
+
+PC  ENDS
+        END     START
